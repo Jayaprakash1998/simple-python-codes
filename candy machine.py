4bbca77d3b792392c68405e4187ad97f203689ef
JAYAPRAKASH <55060090+Jayaprakash1998@users.noreply.github.com>
2020-04-03 06:57:43 -0700

    Create candy machine.py

---
+++ candy machine.py
@@ -0,0 +1,12 @@
+av = 3
+x=int(input("enter no of candies you want :"))
+i=1
+while i<=x:
+    if i>av:
+        print("out of stock")
+        break
+
+    print("candy")
+    i=i+1
+
+print("Bye")
