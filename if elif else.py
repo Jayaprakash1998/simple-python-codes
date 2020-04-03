3916b0d266aadba0c85d1fbf2e35991a827f1200
JAYAPRAKASH <55060090+Jayaprakash1998@users.noreply.github.com>
2020-04-03 06:56:00 -0700

    Create if elif else.py

---
+++ if elif else.py
@@ -0,0 +1,15 @@
+x=int(input("enter a number:"))
+if x==1:
+    print("ONE")
+
+elif x==2:
+    print("TWO")
+
+elif x==3:
+    print("THREE")
+
+elif x==4:
+    print("FOUR")
+
+else:
+    print("SORRY")
